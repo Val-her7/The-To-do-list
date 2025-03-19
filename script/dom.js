49fd79da5ed03a5e3dddf4e20ab10da0fd0b8ab1
Valher7 <Valentin.herman.pro@gmail.com>
2025-03-19 21:19:43 +0100

    Link input with label

--- script/dom.js
+++ script/dom.js
@@ -2,9 +2,9 @@ export function createNewTask(task) {
 
     const LI = document.createElement("li");
     LI.innerHTML = `
-    <input type="checkbox">
-    <label>${task}</label>
-    <button>Delete</button>`
+    <input type="checkbox" id="${task}">
+    <label for="${task}">${task}</label>
+    <button style="visibility:hidden">Delete</button>`
 
     return LI; 
 }
