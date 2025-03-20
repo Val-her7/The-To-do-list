2c104307dd32859d0975834453246ea33a6aa9e3
Valher7 <Valentin.herman.pro@gmail.com>
2025-03-20 16:46:41 +0100

    Add class name to li, checked attribute and class for button

--- script/dom.js
+++ script/dom.js
@@ -1,10 +1,13 @@
-export function createNewTask(task, id) {
+export function createNewTask(task, id, completed) {
 
     const LI = document.createElement("li");
+    LI.classList.add("main__task");
+    const STATUS = completed ? "checked": "";
+    const VISIBLE = completed ? "btn-visible": "btn-invisible";
     LI.innerHTML = `
-    <input type="checkbox" id="${task}">
+    <input type="checkbox" id="${task}" ${STATUS}>
     <label for="${task}">${task}</label>
-    <button id="${id}" style="visibility:hidden">Delete</button>`
+    <button id="${id}" class="${VISIBLE}">Delete</button>`
     
     return LI; 
 }
